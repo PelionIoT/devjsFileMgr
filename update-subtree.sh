e51983f81ca79c73cc97118571e5030cf00bea60
edhemphill <edhemphill@wigwag.com>
2015-08-31 17:00:58 -0500

    update subtree script

---
+++ update-subtree.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SELF="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+SELF="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+pushd $SELF > /dev/null
+echo "Updating jsDav"
+git pull -s subtree jsdav master
+popd > /dev/null
